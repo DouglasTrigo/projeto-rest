3a3028aa6d751f956d2058b72ba145df80bc1d23
Douglas Gil Santos Trigo <douglas.trigo@hotmail.com>
2020-12-07 23:07:01 -0300

    Correção

--- src/main/java/com/prova/apirest/client/AleatorioClient.java
+++ src/main/java/com/prova/apirest/client/AleatorioClient.java
@@ -2,15 +2,12 @@ package com.prova.apirest.client;
 
 import com.prova.apirest.dto.response.AleatorioResponse;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
 @Component
 public class AleatorioClient {
 
-    @GetMapping("/api/deck/new/shuffle/")
-    public AleatorioResponse getAleatorioResponse(@RequestParam("deck_count") Integer deckCount){
+    public AleatorioResponse getAleatorioResponse(Integer deckCount){
         var restTemplate = new RestTemplate();
         String url = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=" + deckCount;
         return restTemplate.getForEntity(url, AleatorioResponse.class).getBody();
